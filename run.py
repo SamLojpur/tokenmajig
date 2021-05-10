a926f742bfb1ae0ccb103d816e7f8ead24ef7cc8
Samuel Lojpur <samlojpur@gmail.com>
2021-05-09 19:13:31 -0600

    fix size options

--- run.py
+++ run.py
@@ -5,21 +5,6 @@ import subprocess
 import urllib.request
 import os
 
-card = requests.get(
-    'https://api.scryfall.com/cards/named?fuzzy=Wasitora').json()
-print("Ok")
-for token in [card_part for card_part in card['all_parts'] if card_part['component'] == 'token']:
-    token_image = requests.get(token['uri']).json()[
-        'image_uris']['border_crop']
-    print(token_image)
-    urllib.request.urlretrieve(
-        token_image, "token.jpg")
-    # lp = subprocess.Popen([
-    #     "/usr/bin/lp", "-o", "PageSize=X72MMY100MM", "-o", "PrintSpeed=2Low", "-o", "portrait", "-d", "PDF", "token.jpg"])
-    print_image(size=1)
-    # lp = subprocess.Popen([
-    #     "/usr/bin/lp", "-o", "PageSize=X72MMY50MM", "-o", "PrintSpeed=2Low", "-o", "landscape", "-d", "PDF", "token.jpg"])
-
 
 def print_image(size=0):
     argslist = [
@@ -38,7 +23,7 @@ def print_image(size=0):
         argslist.append("-o")
         argslist.append("landscape")
 
-    if size == 1:
+    elif size == 1:
         argslist.append("-o")
         argslist.append("PageSize=X72MMY50MM")
         argslist.append("-o")
@@ -46,7 +31,7 @@ def print_image(size=0):
         argslist.append("-o")
         argslist.append("scaling=85")
 
-    else if size == 2:
+    elif size == 2:
         argslist.append("-o")
         argslist.append("PageSize=X72MMY100MM")
         argslist.append("-o")
@@ -54,3 +39,18 @@ def print_image(size=0):
 
     argslist.append("token.jpg")
     lp = subprocess.Popen(argslist)
+
+
+card = requests.get(
+    'https://api.scryfall.com/cards/named?fuzzy=Wasitora').json()
+for token in [card_part for card_part in card['all_parts'] if card_part['component'] == 'token']:
+    token_image = requests.get(token['uri']).json()[
+        'image_uris']['border_crop']
+    print(token_image)
+    urllib.request.urlretrieve(
+        token_image, "token.jpg")
+    # lp = subprocess.Popen([
+    #     "/usr/bin/lp", "-o", "PageSize=X72MMY100MM", "-o", "PrintSpeed=2Low", "-o", "portrait", "-d", "PDF", "token.jpg"])
+    print_image(size=1)
+    # lp = subprocess.Popen([
+    #     "/usr/bin/lp", "-o", "PageSize=X72MMY50MM", "-o", "PrintSpeed=2Low", "-o", "landscape", "-d", "PDF", "token.jpg"])
